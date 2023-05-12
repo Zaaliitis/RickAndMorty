7cc313992ef49f99ee75d3453498272cb84f7735
Zalitis <zrobertss@gmail.com>
2023-05-12 16:35:23 +0300

    Adding files

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require '../vendor/autoload.php';
 use App\Core\Router;
 
 $response = Router::response();
-$render = new \App\Core\Renderer('/../app/Views');
+$render = new \App\Core\Renderer('/../Views');
 echo $render->render($response);
 
 
